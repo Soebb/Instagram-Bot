f918800ca37a6583895921ae7548544690d4e238
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-31 21:09:25 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os, pickle
-from instaloader import Instaloader
+from instaloader import Instaloader, ConnectionException
 from dotenv import load_dotenv
 #from selenium.webdriver import Chrome
 from selenium.webdriver.firefox.options import Options
@@ -13,6 +13,42 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
 #from selenium.webdriver.firefox.service import Service as FirefoxService
 import geckodriver_autoinstaller
+from glob import glob
+from os.path import expanduser
+from platform import system
+from sqlite3 import OperationalError, connect
+
+
+def get_cookiefile():
+    default_cookiefile = {
+        "Windows": "~/AppData/Roaming/Mozilla/Firefox/Profiles/*/cookies.sqlite",
+        "Darwin": "~/Library/Application Support/Firefox/Profiles/*/cookies.sqlite",
+    }.get(system(), "~/.mozilla/firefox/*/cookies.sqlite")
+    cookiefiles = glob(expanduser(default_cookiefile))
+    if not cookiefiles:
+        raise SystemExit("No Firefox cookies.sqlite file found. Use -c COOKIEFILE.")
+    return cookiefiles[0]
+
+
+def import_session(cookiefile, sessionfile):
+    print("Using cookies from {}.".format(cookiefile))
+    conn = connect(f"file:{cookiefile}?immutable=1", uri=True)
+    try:
+        cookie_data = conn.execute(
+            "SELECT name, value FROM moz_cookies WHERE baseDomain='instagram.com'"
+        )
+    except OperationalError:
+        cookie_data = conn.execute(
+            "SELECT name, value FROM moz_cookies WHERE host LIKE '%instagram.com'"
+        )
+    instaloader = Instaloader(max_connection_attempts=1)
+    instaloader.context._session.cookies.update(cookie_data)
+    username = instaloader.test_login()
+    if not username:
+        raise SystemExit("Not logged in. Are you logged in successfully in Firefox?")
+    print("Imported session cookie for {}.".format(username))
+    instaloader.context.username = username
+    instaloader.save_session_to_file(sessionfile)
 
 
 geckodriver_autoinstaller.install() # if it doesn't exist, download it automatically,
